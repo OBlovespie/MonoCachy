5e1e323d8d26708be0d95fa6848b5694fc69d2e2
OBlovespie <charlieduignan@yahoo.com>
2026-08-04 20:58:39 +0100

    Update play_sound.sh

--- swaync/play_sound.sh
+++ swaync/play_sound.sh
@@ -1,6 +1,12 @@
-
 #!/usr/bin/env bash
+SOUND_FILE="/home/fm1/Music/lbpnotification.mp3"
 
-SOUND_FILE="~/.config/swaync/lbpnotification.mp3"
+
+dnd_state="$(swaync-client -D 2>/dev/null | tr -d '\r\n' | tr '[:upper:]' '[:lower:]')"
+
+
+if echo "$dnd_state" | grep -Eq '^(1|on|true|enabled)$'; then
+  exit 0
+fi
 
 mpv --no-video --really-quiet "$SOUND_FILE"
